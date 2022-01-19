f962c1e8aba112e16624a81f07c2195653c94501
sashaz <6964653+OZahnitko@users.noreply.github.com>
2022-01-19 23:10:35 +1000

    asd

--- packages/scripts/src/determineChanges.ts
+++ packages/scripts/src/determineChanges.ts
@@ -3,12 +3,30 @@ import { promisify } from "util";
 
 const exec = promisify(syncExec);
 
-const findChangedFiles = async () => {
+export const findChangedFiles = async (): Promise<string[]> => {
   const { stdout } = await exec(`
         cat changes.txt
     `);
+
   console.log("The following files have changed since the last commit...");
-  console.log(stdout.split("\n").filter((path) => !!path));
+
+  return stdout.split("\n").filter((path) => !!path);
 };
 
-findChangedFiles();
+// findChangedFiles();
+
+export const filterChangedFiles = (
+  fileList: string[]
+): { packageChanges: string[] } => {
+  const packageChanges = fileList
+    .filter((filePath) => filePath.includes("packages"))
+    .map((file) => file.split("/").slice(1).join("/"));
+
+  return { packageChanges };
+};
+
+(async () => {
+  const changes = await findChangedFiles();
+  console.log(changes);
+  console.log(filterChangedFiles(changes));
+})();
