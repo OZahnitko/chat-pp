f21f7b33ad97d09c56bc83b8d9de096d7edbe2d7
sashaz <6964653+OZahnitko@users.noreply.github.com>
2022-01-19 21:34:38 +1000

    ye

---
+++ scripts/determineChanges.js
@@ -0,0 +1,14 @@
+const { exec: syncExec } = require("child_process");
+const { promisify } = require("util");
+
+const exec = promisify(syncExec);
+
+const findChanges = async () => {
+  const { stdout } = await exec(
+    "git diff --name-only ${{ github.event.before }}..${{ github.event.afters }}"
+  );
+
+  console.log(stdout);
+};
+
+findChanges();
